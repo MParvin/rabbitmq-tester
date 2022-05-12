22f66659241fa62064ef61b1566e4c46be584e00
Mohammad Parvin <m.parvin986@gmail.com>
2022-05-12 15:48:53 +0430

    cobra with commands added

---
+++ cmd/send.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// sendCmd represents the send command
+var sendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "This command will send messages to a queue",
+	Long: `To send a message to a queue, use the following command:
+
+rabbitmq-test -c /PATH/TO/CONFIG_FILE send`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("send called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sendCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// sendCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// sendCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
