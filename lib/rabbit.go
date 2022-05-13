7609e85a5057796f40a84dc1627ff69c9318be55
Mohammad Parvin <m.parvin986@gmail.com>
2022-05-13 18:31:29 +0430

    add rabbit liberary to connect and write to rabbit, and read from it

---
+++ lib/rabbit.go
@@ -0,0 +1,90 @@
+package lib
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func closeRabbit(conn *amqp.Connection) {
+	err := conn.Close()
+	failOnError(err, "Failed to close RabbitMQ connection")
+}
+
+func Send(rabbithost string, queue string, message string) {
+	conn, err := amqp.Dial(rabbithost)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		queue, // name
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s", message)
+}
+
+func Receive(rabbithost string, queue string, message string) {
+	conn, err := amqp.Dial(rabbithost)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		queue, // name
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	for d := range msgs {
+		log.Printf(" [x] Received %s", d.Body)
+		if string(d.Body) == message {
+			log.Printf(" [x] Received %s", message)
+			break
+		}
+	}
+}
